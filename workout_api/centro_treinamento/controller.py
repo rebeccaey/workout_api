5512dd51d44ccd3f1001a3c9f805ef623907a73f
Rebecca <111652786+rebeccaey@users.noreply.github.com>
2024-07-13 14:34:39 -0300

    Update controller.py

--- workout_api/centro_treinamento/controller.py
+++ workout_api/centro_treinamento/controller.py
@@ -1,9 +1,11 @@
 from uuid import uuid4
-from fastapi import APIRouter, Body, HTTPException, status
+from typing import List
+from fastapi import APIRouter, Body, HTTPException, Query, status
 from pydantic import UUID4
+from fastapi_pagination import PaginationParams, Page
+from sqlalchemy.exc import IntegrityError
 from workout_api.centro_treinamento.schemas import CentroTreinamentoIn, CentroTreinamentoOut
 from workout_api.centro_treinamento.models import CentroTreinamentoModel
-
 from workout_api.contrib.dependencies import DatabaseDependency
 from sqlalchemy.future import select
 
@@ -19,28 +21,47 @@ async def post(
     db_session: DatabaseDependency, 
     centro_treinamento_in: CentroTreinamentoIn = Body(...)
 ) -> CentroTreinamentoOut:
-    centro_treinamento_out = CentroTreinamentoOut(id=uuid4(), **centro_treinamento_in.model_dump())
-    centro_treinamento_model = CentroTreinamentoModel(**centro_treinamento_out.model_dump())
+    try:
+        centro_treinamento_out = CentroTreinamentoOut(id=str(uuid4()), **centro_treinamento_in.model_dump())
+        centro_treinamento_model = CentroTreinamentoModel(**centro_treinamento_out.model_dump())
+        
+        db_session.add(centro_treinamento_model)
+        await db_session.commit()
+        
+        return centro_treinamento_out
     
-    db_session.add(centro_treinamento_model)
-    await db_session.commit()
+    except IntegrityError as e:
+        # Verifica se a exceção ocorreu devido a violação de integridade (exemplo: nome duplicado)
+        if "duplicate key value violates unique constraint" in str(e):
+            raise HTTPException(
+                status_code=status.HTTP_303_SEE_OTHER,
+                detail=f'Já existe um centro de treinamento cadastrado com o nome: {centro_treinamento_in.nome}'
+            )
+        else:
+            raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail='Ocorreu um erro ao inserir os dados no banco'
+            )
 
-    return centro_treinamento_out
-    
-    
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f'Ocorreu um erro inesperado: {str(e)}'
+        )
+
 @router.get(
     '/', 
     summary='Consultar todos os centros de treinamento',
     status_code=status.HTTP_200_OK,
-    response_model=list[CentroTreinamentoOut],
+    response_model=Page[CentroTreinamentoOut],
 )
-async def query(db_session: DatabaseDependency) -> list[CentroTreinamentoOut]:
-    centros_treinamento_out: list[CentroTreinamentoOut] = (
-        await db_session.execute(select(CentroTreinamentoModel))
-    ).scalars().all()
-    
-    return centros_treinamento_out
-
+async def query(
+    db_session: DatabaseDependency,
+    params: PaginationParams = PaginationParams()
+) -> Page[CentroTreinamentoOut]:
+    query = select(CentroTreinamentoModel)
+    centros_treinamento = await paginate(db_session, query, params=params)
+    return centros_treinamento
 
 @router.get(
     '/{id}', 
